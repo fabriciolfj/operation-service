eb372c70005a6be98fb9ec7fed1c89d4e328cf03
fabricio jacob <fabricio.jacob@outlook.com>
2022-07-30 23:14:29 -0300

    add

--- src/main/java/com/github/fabriciolfj/entrypoint/controller/TransactionController.java
+++ src/main/java/com/github/fabriciolfj/entrypoint/controller/TransactionController.java
@@ -1,6 +1,7 @@
 package com.github.fabriciolfj.entrypoint.controller;
 
 import com.github.fabriciolfj.business.usecase.TransactionCreateCase;
+import com.github.fabriciolfj.business.usecase.TransactionUpdateCase;
 import com.github.fabriciolfj.entrypoint.converter.TransactionRequestConveter;
 import com.github.fabriciolfj.entrypoint.dto.TransactionRequest;
 import com.github.fabriciolfj.entrypoint.dto.TransactionResponse;
@@ -8,9 +9,7 @@ import io.smallrye.mutiny.Uni;
 import lombok.RequiredArgsConstructor;
 
 import javax.validation.Valid;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
 @Path("/api/v1/transactions")
@@ -18,6 +17,7 @@ import javax.ws.rs.core.Response;
 public class TransactionController {
 
     private final TransactionCreateCase transactionCreateCase;
+    private final TransactionUpdateCase transactionUpdateCase;
 
     @POST
     @Path("/debit")
@@ -27,5 +27,13 @@ public class TransactionController {
                 .ifNull().failWith(new WebApplicationException("Fail save transaction", Response.Status.BAD_REQUEST))
                 .onItem().ifNotNull().transform(c -> Response.accepted().entity(TransactionResponse.builder().transaction(c).build()).build())
                 .onFailure().recoverWithItem(e -> Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build());
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Uni<Response> updateTransaction(@PathParam("id") final String id) {
+        return transactionUpdateCase.execute(id)
+                .onItem().transform(t -> Response.accepted().build())
+                .onFailure().recoverWithItem(t -> Response.status(Response.Status.BAD_REQUEST).entity(t.getMessage()).build());
     }
 }
