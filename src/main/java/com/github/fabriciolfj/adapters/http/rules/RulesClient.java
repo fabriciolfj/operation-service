441c7919bae775712475801db225c9c6773fc2d0
fabricio jacob <fabricio.jacob@outlook.com>
2022-08-13 18:17:46 -0300

    add

--- src/main/java/com/github/fabriciolfj/adapters/http/rules/RulesClient.java
+++ src/main/java/com/github/fabriciolfj/adapters/http/rules/RulesClient.java
@@ -1,10 +1,18 @@
 package com.github.fabriciolfj.adapters.http.rules;
 
+import io.smallrye.mutiny.Uni;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import java.math.BigDecimal;
 
 @Path("/api/v1/loan")
 @RegisterRestClient(configKey = "rules-api")
 public interface RulesClient {
+
+    @GET
+    @Path("/{value}")
+    Uni<BigDecimal> findLoan(@PathParam("value") final BigDecimal value);
 }
